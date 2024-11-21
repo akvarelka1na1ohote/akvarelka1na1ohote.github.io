f354b75ba274f074d8a12bf7236932743de99f9b
akvarelka1na1ohote <166593870+akvarelka1na1ohote@users.noreply.github.com>
2024-11-21 13:55:09 +0300

    Сайт на 21.11.2024

---
+++ review.js
@@ -0,0 +1,51 @@
+const advantagesData2 = [
+    {
+        imageScr: "images/review_1.png",
+    }, 
+    {
+        imageScr: "images/review_2.png",
+    }, 
+    {
+        imageScr: "images/review_3.png",
+    }, 
+    {
+        imageScr: "images/review_4.png",
+        
+    }, 
+    {
+        imageScr: "images/review_5.png",
+        
+    }, 
+    {
+        imageScr: "images/review_6.png",
+    }, 
+    
+]
+
+var current_start_index_2 = 0;
+
+
+
+var review_carusel_1 = document.getElementById('review_carusel_1');
+var review_carusel_2 = document.getElementById('review_carusel_2');
+var review_carusel_3 = document.getElementById('review_carusel_3');
+
+var carusel_review_left = document.getElementById('carusel_review_left');
+var carusel_review_right = document.getElementById('carusel_review_right');
+
+
+function updateCaruselReview () {
+    review_carusel_1.src = advantagesData2[current_start_index_2 %6].imageScr;
+    review_carusel_2.src = advantagesData2[(current_start_index_2+1)%6].imageScr;
+    review_carusel_3.src = advantagesData2[(current_start_index_2+2)%6].imageScr;
+}
+
+carusel_review_left.onclick = function() {
+    current_start_index = (current_start_index_2-1+6);
+    updateCaruselReview();
+}
+
+carusel_review_right.onclick = function() {
+    current_start_index = (current_start_index_2+1);
+    updateCaruselReview();
+}
